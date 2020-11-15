b442bc660e0b376a235c9136cd6e36d5918d2488
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-11-15 20:38:17 +0100

    add local-gnome-shell-extension-version

--- docs/_modules/gnome_shell.md
+++ docs/_modules/gnome_shell.md
@@ -2,10 +2,16 @@
 title: GNOME Shell
 ---
 
-| Instruction             | Action                        | Arguments          |
-| ----------------------- | ----------------------------- | ------------------ |
-| `gnome-shell-extension` | Enable GNOME Shell extensions | List of extensions |
-| `gnome-shell-restart`   | Restart GNOME Shell           | none               |
+| Instruction                           | Action                                                         | Arguments                       |
+| ------------------------------------- | -------------------------------------------------------------- | ------------------------------- |
+| `gnome-shell-extension`               | Enable GNOME Shell extensions                                  | List of extensions              |
+| `local-gnome-shell-extension-version` | Check the version of a locally-installed GNOME Shell extension | Parameters as a map (see below) |
+| `gnome-shell-restart`                 | Restart GNOME Shell                                            | none                            |
+
+Parameters for version checking:
+
+- `extension` (mandatory): name of the extension
+- `store`: key name in the store (if not provided, the version is not stored)
 
 Example:
 
@@ -14,4 +20,6 @@ Example:
     - gnome-shell-extension:
         - native-window-placement@gnome-shell-extensions.gcampax.github.com
         - places-menu@gnome-shell-extensions.gcampax.github.com
+    - local-gnome-shell-extension-version:
+        extension: places-menu@gnome-shell-extensions.gcampax.github.com
 ```
--- internal/modules/gnome_shell.go
+++ internal/modules/gnome_shell.go
@@ -1,12 +1,18 @@
 package modules
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+
 	"github.com/tiramiseb/quickonf/internal/helper"
 	"github.com/tiramiseb/quickonf/internal/output"
 )
 
 func init() {
 	Register("gnome-shell-extension", GnomeShellExtension)
+	Register("local-gnome-shell-extension-version", LocalGnomeShellExtensionVersion)
 	Register("gnome-shell-restart", GnomeShellRestart)
 }
 
@@ -26,24 +32,65 @@ func GnomeShellExtension(in interface{}, out output.Output) error {
 	return nil
 }
 
+// LocalGnomeShellExtensionVersion chekcs a locally installed GNOME Shell extension version
+func LocalGnomeShellExtensionVersion(in interface{}, out output.Output) error {
+	out.InstructionTitle("Checking GNOME Shell extension version")
+	data, err := helper.MapStringString(in)
+	if err != nil {
+		return err
+	}
+	ext, ok := data["extension"]
+	if !ok {
+		return errors.New("Missing extension name")
+	}
+	f, err := os.Open(helper.Path(fmt.Sprintf(".local/share/gnome-shell/extensions/%s/metadata.json", ext)))
+
+	storeVersion := func(val string) {
+		if storeAs, ok := data["store"]; ok {
+			helper.Store(storeAs, val)
+		}
+	}
+
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			out.Info("Extension " + ext + " is not installed")
+			storeVersion("")
+			return nil
+		}
+		return err
+	}
+	defer f.Close()
+	jsondec := json.NewDecoder(f)
+	extMeta := map[string]interface{}{}
+	jsondec.Decode(&extMeta)
+	version, ok := extMeta["version"]
+	if !ok {
+		out.Info("Extension " + ext + " does not declare a version")
+		storeVersion("")
+		return nil
+	}
+	switch val := version.(type) {
+	case string:
+		out.Info(fmt.Sprintf("Extension %s version is %s", ext, val))
+		storeVersion(val)
+	case int:
+		out.Info(fmt.Sprintf("Extension %s version is %d.0.0", ext, val))
+		storeVersion(fmt.Sprintf("%d.0.0", val))
+	case float64:
+		out.Info(fmt.Sprintf("Extension %s version is %.f.0.0", ext, val))
+		storeVersion(fmt.Sprintf("%.f.0.0", val))
+	default:
+		out.Alert(fmt.Sprintf("Extension %s version is not understood", ext))
+		storeVersion("")
+	}
+	return nil
+}
+
 // GnomeShellRestart restarts GNOME Shell
 func GnomeShellRestart(in interface{}, out output.Output) error {
 	out.InstructionTitle("Restart GNOME Shell")
 	if _, err := helper.Exec("busctl", "--user", "call", "org.gnome.Shell", "/org/gnome/Shell", "org.gnome.Shell", "Eval", "s", `Meta.restart("Restarting Gnome...")`); err != nil {
 		return err
 	}
-	// procs, err := process.Processes()
-	// if err != nil {
-	// 	return err
-	// }
-	// for _, p := range procs {
-	// 	n, err := p.Name()
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	if n == "gnome-shell" {
-	// 		p.SendSignal(syscall.SIGQUIT)
-	// 	}
-	// }
 	return nil
 }
