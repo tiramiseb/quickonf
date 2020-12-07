89f424f3e62a978e0d51d13c0298e292635a7426
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-12-07 09:28:36 +0100

    add xdg-autostart

--- docs/_modules/xdg.md
+++ docs/_modules/xdg.md
@@ -4,14 +4,21 @@ title: XDG
 
 | Instruction        | Action                                 | Arguments                             |
 | ------------------ | -------------------------------------- | ------------------------------------- |
+| `xdg-autostart`    | Set autostart for applications         | List of app names or .desktop files   |
 | `xdg-mime-default` | Set default applications for mimetypes | Map from mimetype to application name |
 | `xdg-user-dir`     | Set a XDG user dir                     | Map from name to path                 |
 
-Application name is the `.desktop` file name without the extension.
+For `xdg-autostart`, a ".desktop file" is the path to the `.desktop` file, especially if it is not in `/usr/share/applications`. If missing, the `.desktop` extension is automatically added.
+
+For `xdg-autostart` and `xdg-mime-default`, application name is the `.desktop` file name without the extension.
 
 Example:
 
 ```yaml
+- Seafile autostart:
+    - xdg-autostart:
+        - seafile
+
 - Inkscape:
     - xdg-mime-default:
         image/svg+xml: inkscape
--- internal/modules/xdg.go
+++ internal/modules/xdg.go
@@ -2,26 +2,72 @@ package modules
 
 import (
 	"errors"
+	"os"
+	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/tiramiseb/quickonf/internal/helper"
 	"github.com/tiramiseb/quickonf/internal/output"
 )
 
-var xdgAllUserDirs = map[string]bool{
-	"DESKTOP":     true,
-	"DOCUMENTS":   true,
-	"DOWNLOAD":    true,
-	"MUSIC":       true,
-	"PICTURES":    true,
-	"PUBLICSHARE": true,
-	"TEMPLATES":   true,
-	"VIDEOS":      true,
-}
+var (
+	xdgApplicationsDir = "/usr/share/applications"
+	xdgAutostartDir    = ".config/autostart"
+	xdgAllUserDirs     = map[string]bool{
+		"DESKTOP":     true,
+		"DOCUMENTS":   true,
+		"DOWNLOAD":    true,
+		"MUSIC":       true,
+		"PICTURES":    true,
+		"PUBLICSHARE": true,
+		"TEMPLATES":   true,
+		"VIDEOS":      true,
+	}
+)
 
 func init() {
+	Register("xdg-autostart", XdgAutostart)
 	Register("xdg-mime-default", XdgMimeDefault)
 	Register("xdg-user-dir", XdgUserDir)
+}
+
+// XdgAutostart enables autostart for given applications
+func XdgAutostart(in interface{}, out output.Output) error {
+	out.InstructionTitle("XDG autostart")
+	data, err := helper.SliceString(in)
+	if err != nil {
+		return err
+	}
+	for _, app := range data {
+		appBase := app
+		if !strings.HasSuffix(appBase, ".desktop") {
+			appBase = app + ".desktop"
+		}
+		fullApp := appBase
+		if !path.IsAbs(fullApp) {
+			if _, err := os.Stat(filepath.Join(xdgApplicationsDir, fullApp)); !errors.Is(err, os.ErrNotExist) {
+				fullApp = filepath.Join(xdgApplicationsDir, fullApp)
+			}
+		}
+		fullApp = helper.Path(fullApp)
+		autostartPath := helper.Path(filepath.Join(xdgAutostartDir, filepath.Base(appBase)))
+
+		status, err := helper.Symlink(autostartPath, fullApp)
+		switch status {
+		case helper.SymlinkError:
+			return err
+		case helper.SymlinkAleradyExists:
+			out.Info("Autostart for " + app + " already configured")
+		case helper.SymlinkCreated:
+			if Dryrun {
+				out.Info("Would autostart " + app)
+			} else {
+				out.Success("Autostart for " + app + " configured")
+			}
+		}
+	}
+	return nil
 }
 
 // XdgMimeDefault sets default applications for mimetypes
