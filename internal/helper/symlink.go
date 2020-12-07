0dd19cc43527d1514bc56e44307368259f8e390b
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-12-07 09:28:10 +0100

    have a symlink helper

---
+++ internal/helper/symlink.go
@@ -0,0 +1,46 @@
+package helper
+
+import (
+	"errors"
+	"os"
+)
+
+// SymlinkStatus is the status after trying to create a symbolic link
+type SymlinkStatus int
+
+// Symlink status is returned when trying to create a symbolic link
+const (
+	SymlinkError SymlinkStatus = iota
+	SymlinkAleradyExists
+	SymlinkCreated
+)
+
+// Symlink creates a symbolic link
+func Symlink(path, target string) (status SymlinkStatus, err error) {
+	if stat, err := os.Lstat(target); err == nil {
+		if stat.Mode()&os.ModeSymlink != os.ModeSymlink {
+			return SymlinkError, errors.New(path + " already exists but is not a symlink")
+		}
+		if currentTarget, err := os.Readlink(path); err == nil {
+			if currentTarget == target {
+				return SymlinkAleradyExists, nil
+			}
+			if !Dryrun {
+				if err := os.Remove(path); err != nil {
+					return SymlinkError, err
+				}
+			}
+		} else if !os.IsNotExist(err) {
+			return SymlinkError, err
+		}
+
+	} else if !os.IsNotExist(err) {
+		return SymlinkError, err
+	}
+	if !Dryrun {
+		if err := os.Symlink(target, path); err != nil {
+			return SymlinkError, err
+		}
+	}
+	return SymlinkCreated, nil
+}
--- internal/modules/symlink.go
+++ internal/modules/symlink.go
@@ -1,9 +1,6 @@
 package modules
 
 import (
-	"errors"
-	"os"
-
 	"github.com/tiramiseb/quickonf/internal/helper"
 	"github.com/tiramiseb/quickonf/internal/output"
 )
@@ -22,37 +19,19 @@ func Symlink(in interface{}, out output.Output) error {
 	for path, target := range data {
 		path = helper.Path(path)
 		target = helper.Path(target)
-		if stat, err := os.Lstat(target); err == nil {
-			if stat.Mode()&os.ModeSymlink != os.ModeSymlink {
-				return errors.New(path + " already exists but is not a symlink")
-			}
-			if currentTarget, err := os.Readlink(path); err == nil {
-				if currentTarget == target {
-					out.Info("Link from " + path + " to " + target + " already exists")
-					return nil
-				}
-				if Dryrun {
-					out.Info("Would remove " + path)
-				} else {
-					if err := os.Remove(path); err != nil {
-						return err
-					}
-				}
-			} else if !os.IsNotExist(err) {
-				return err
-			}
-
-		} else if !os.IsNotExist(err) {
+		status, err := helper.Symlink(path, target)
+		switch status {
+		case helper.SymlinkError:
 			return err
+		case helper.SymlinkAleradyExists:
+			out.Info("Link from " + path + " to " + target + " already exists")
+		case helper.SymlinkCreated:
+			if Dryrun {
+				out.Info("Would create link from " + path + " to " + target)
+			} else {
+				out.Success("Link from " + path + " to " + target + " created")
+			}
 		}
-		if Dryrun {
-			out.Info("Would create link from " + path + " to " + target)
-			continue
-		}
-		if err := os.Symlink(target, path); err != nil {
-			return err
-		}
-		out.Success("Link from " + path + " to " + target + " created")
 	}
 	return nil
 }
