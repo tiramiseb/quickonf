f82861dfd6e2f0d323201de1efcb5bdc9c3a3671
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-12-22 18:47:31 +0100

    add install-gnome-shell-extension
    which implies renaming gnome-shell-extension to enable-gnome-shell-extension

--- docs/_modules/gnome_shell.md
+++ docs/_modules/gnome_shell.md
@@ -4,9 +4,12 @@ title: GNOME Shell
 
 | Instruction                           | Action                                                         | Arguments                       |
 | ------------------------------------- | -------------------------------------------------------------- | ------------------------------- |
-| `gnome-shell-extension`               | Enable GNOME Shell extensions                                  | List of extensions              |
+| `install-gnome-shell-extension`       | Install GNOME Shell extensions from extensions.gnome.org       | List of extensions              |
+| `enable-gnome-shell-extension`        | Enable GNOME Shell extensions                                  | List of extensions              |
 | `local-gnome-shell-extension-version` | Check the version of a locally-installed GNOME Shell extension | Parameters as a map (see below) |
 | `gnome-shell-restart`                 | Restart GNOME Shell                                            | none                            |
+
+Extensions names are their UUIDs, like "places-menu@gnome-shell-extensions.gcampax.github.com".
 
 Parameters for version checking:
 
@@ -17,7 +20,7 @@ Example:
 
 ```yaml
 - GNOME Shell extensions:
-    - gnome-shell-extension:
+    - enable-gnome-shell-extension:
         - native-window-placement@gnome-shell-extensions.gcampax.github.com
         - places-menu@gnome-shell-extensions.gcampax.github.com
     - local-gnome-shell-extension-version:
--- internal/modules/gnome_shell.go
+++ internal/modules/gnome_shell.go
@@ -1,9 +1,11 @@
 package modules
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strconv"
 
@@ -12,13 +14,81 @@ import (
 )
 
 func init() {
-	Register("gnome-shell-extension", GnomeShellExtension)
+	Register("install-gnome-shell-extension", InstallGnomeShellExtension)
+	Register("enable-gnome-shell-extension", EnableGnomeShellExtension)
 	Register("local-gnome-shell-extension-version", LocalGnomeShellExtensionVersion)
 	Register("gnome-shell-restart", GnomeShellRestart)
 }
 
-// GnomeShellExtension enables GNOME Shell extensions
-func GnomeShellExtension(in interface{}, out output.Output) error {
+// InstallGnomeShellExtension installs GNOME Shell extensions from extensions.gnome.org
+func InstallGnomeShellExtension(in interface{}, out output.Output) error {
+	out.InstructionTitle("Install GNOME Shell extension")
+	data, err := helper.SliceString(in)
+	if err != nil {
+		return err
+	}
+	cmdout, err := helper.Exec(nil, "gnome-shell", "--version")
+	if err != nil {
+		return err
+	}
+	gnomeVerLine := bytes.Fields(cmdout)
+	if len(gnomeVerLine) != 3 {
+		return fmt.Errorf(`Gnome version invalid, should be "GNOME Shell X.Y.Z": %s`, cmdout)
+	}
+	gnomeVersion := string(gnomeVerLine[2])
+	for _, ext := range data {
+		dest, err := helper.String(helper.Path(".local/share/gnome-shell/extensions/" + ext))
+		if err != nil {
+			return err
+		}
+		out.ShowLoader()
+		extInfo := struct {
+			Version     int
+			DownloadURL string `json:"download_url"`
+		}{}
+		err = helper.DownloadJSON("https://extensions.gnome.org/extension-info/?uuid="+ext+"&shell_version="+gnomeVersion, &extInfo)
+		out.HideLoader()
+		if err != nil {
+			if err.Error() == "404 not found" {
+				return fmt.Errorf("Extension %s does not exist", ext)
+			}
+			return err
+		}
+
+		_, current, err := localGnomeShellExtensionVersion(ext)
+		if err != nil {
+			return err
+		}
+		if extInfo.Version <= current {
+			out.Info(fmt.Sprintf("%s already installed in version %d", ext, current))
+			continue
+		}
+		if Dryrun {
+			out.Info(fmt.Sprintf("Would install %s in version %d", ext, extInfo.Version))
+			continue
+		}
+
+		out.Info("Installing " + ext)
+		tmpfile, err := ioutil.TempFile("", "quickonf-gnome-extension-"+ext+"-*.zip")
+		if err != nil {
+			return err
+		}
+		fname := tmpfile.Name()
+		if err := tmpfile.Close(); err != nil {
+			return err
+		}
+		if err := helper.DownloadFileWithPercent("https://extensions.gnome.org"+extInfo.DownloadURL, fname, out); err != nil {
+			return err
+		}
+		if err = helper.UnzipFile(fname, dest, out); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// EnableGnomeShellExtension enables GNOME Shell extensions
+func EnableGnomeShellExtension(in interface{}, out output.Output) error {
 	out.InstructionTitle("Enable GNOME Shell extension")
 	data, err := helper.SliceString(in)
 	if err != nil {
@@ -48,31 +118,39 @@ func LocalGnomeShellExtensionVersion(in interface{}, out output.Output) error {
 	if !ok {
 		return errors.New("Missing extension name")
 	}
-	f, err := os.Open(helper.Path(fmt.Sprintf(".local/share/gnome-shell/extensions/%s/metadata.json", ext)))
 
-	storeVersion := func(val string) {
-		if storeAs, ok := data["store"]; ok {
-			helper.Store(storeAs, val)
-		}
+	installed, version, err := localGnomeShellExtensionVersion(ext)
+	if err != nil {
+		return err
 	}
 
+	if installed {
+		out.Info(fmt.Sprintf("Extension %s current version is %d", ext, version))
+	} else {
+		out.Info("Extension " + ext + " is not installed")
+	}
+	helper.Store(data["store"], strconv.Itoa(version))
+	return nil
+}
+
+func localGnomeShellExtensionVersion(ext string) (bool, int, error) {
+	f, err := os.Open(helper.Path(fmt.Sprintf(".local/share/gnome-shell/extensions/%s/metadata.json", ext)))
 	if err != nil {
 		if errors.Is(err, os.ErrNotExist) {
-			out.Info("Extension " + ext + " is not installed")
-			storeVersion("")
-			return nil
+			err = nil
 		}
-		return err
+		return false, 0, err
 	}
 	defer f.Close()
 	jsondec := json.NewDecoder(f)
 	extMeta := struct {
 		Version int
 	}{}
-	jsondec.Decode(&extMeta)
-	out.Info(fmt.Sprintf("Extension %s version is %d", ext, extMeta.Version))
-	storeVersion(strconv.Itoa(extMeta.Version))
-	return nil
+	if err := jsondec.Decode(&extMeta); err != nil {
+		return false, 0, err
+	}
+	return true, extMeta.Version, nil
+
 }
 
 // GnomeShellRestart restarts GNOME Shell
