855ea4883e903240c6e6b7caa788879c1bd1dbf8
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-12-22 18:45:51 +0100

    move oscodename init to another file

--- internal/helper/helper.go
+++ internal/helper/helper.go
@@ -1,5 +1,15 @@
 // Package helper provides helpers for modules, in order to simplify their development
 package helper
 
+import "strings"
+
 // Dryrun allows running instructions without system modification
 var Dryrun = false
+
+func init() {
+	cmdout, err := Exec(nil, "lsb_release", "--codename", "--short")
+	if err != nil {
+		panic(err)
+	}
+	Store("oscodename", strings.TrimSpace(string(cmdout)))
+}
--- internal/helper/store.go
+++ internal/helper/store.go
@@ -1,18 +1,11 @@
 package helper
 
-import "strings"
-
 var store = map[string]string{}
 
-func init() {
-	cmdout, err := Exec(nil, "lsb_release", "--codename", "--short")
-	if err != nil {
-		panic(err)
-	}
-	Store("oscodename", strings.TrimSpace(string(cmdout)))
-}
-
-// Store adds a value to the store
+// Store adds a value to the store. Ignored if key is empty
 func Store(key, value string) {
+	if key == "" {
+		return
+	}
 	store[key] = value
 }
