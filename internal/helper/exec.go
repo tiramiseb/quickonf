0868d33cd7fd70ab2c7522ff22581f4de0eb5343
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2020-12-22 14:53:02 +0100

    add env vars to os.Environ()

--- internal/helper/exec.go
+++ internal/helper/exec.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"errors"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -12,7 +13,7 @@ var SudoPassword string
 // Exec executes a command
 func Exec(env []string, cmd string, args ...string) ([]byte, error) {
 	cmdObj := exec.Command(cmd, args...)
-	cmdObj.Env = append(cmdObj.Env, "LANG=C")
+	cmdObj.Env = append(os.Environ(), "LANG=C")
 	cmdObj.Env = append(cmdObj.Env, env...)
 	cmdout, err := cmdObj.CombinedOutput()
 	if err != nil && len(cmdout) > 0 {
@@ -28,7 +29,7 @@ func ExecSudo(env []string, args ...string) ([]byte, error) {
 	}
 	args = append([]string{"--prompt=", "--stdin"}, args...)
 	sudoCmd := exec.Command("sudo", args...)
-	sudoCmd.Env = append(sudoCmd.Env, "LANG=C")
+	sudoCmd.Env = append(os.Environ(), "LANG=C")
 	sudoCmd.Env = append(sudoCmd.Env, env...)
 	sudoCmd.Stdin = strings.NewReader(SudoPassword)
 	cmdout, err := sudoCmd.CombinedOutput()
