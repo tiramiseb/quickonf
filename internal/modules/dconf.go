dca8ecb3d893f6830550ad242f021577d437b4e5
Sébastien Maccagnoni <sebastien.maccagnoni@eclipse-fr.com>
2021-10-13 14:31:53 +0200

    improve the dconf module, accepting yaml values

--- internal/modules/dconf.go
+++ internal/modules/dconf.go
@@ -1,6 +1,9 @@
 package modules
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/tiramiseb/quickonf/internal/helper"
 	"github.com/tiramiseb/quickonf/internal/output"
 )
@@ -9,10 +12,39 @@ func init() {
 	Register("dconf", Dconf)
 }
 
+func interfaceToDconfValue(src interface{}) string {
+	if asBool, ok := src.(bool); ok {
+		return strconv.FormatBool(asBool)
+	}
+	if asInt, ok := src.(int); ok {
+		return strconv.Itoa(asInt)
+	}
+	if asFloat, ok := src.(float64); ok {
+		return strconv.FormatFloat(asFloat, 'f', -1, 64)
+	}
+	if asString, ok := src.(string); ok {
+		if strings.HasPrefix(asString, "uint32:") {
+			return asString[7:]
+		}
+		return "\"" + strings.ReplaceAll(asString, "\"", "\\\"") + "\""
+	}
+	if asSlice, ok := src.([]interface{}); ok {
+		if len(asSlice) == 0 {
+			return "@as []"
+		}
+		dest := make([]string, len(asSlice))
+		for i, val := range asSlice {
+			dest[i] = interfaceToDconfValue(val)
+		}
+		return "[" + strings.Join(dest, ", ") + "]"
+	}
+	return ""
+}
+
 // Dconf sets a parameter in the dconf database
 func Dconf(in interface{}, out output.Output) error {
 	out.InstructionTitle("Dconf database")
-	data, err := helper.MapStringString(in)
+	data, err := helper.MapStringInterface(in)
 	if err != nil {
 		return err
 	}
@@ -21,10 +53,15 @@ func Dconf(in interface{}, out output.Output) error {
 			out.Infof("Would set %s to %s", k, v)
 			continue
 		}
-		if _, err := helper.Exec(nil, "", "dconf", "write", k, v); err != nil {
+		val := interfaceToDconfValue(v)
+		if val == "" {
+			out.Infof("Could not understand %#v", v)
+			continue
+		}
+		if _, err := helper.Exec(nil, "", "dconf", "write", k, val); err != nil {
 			return err
 		}
-		out.Successf("Set %s to %s", k, v)
+		out.Successf("Set %s to %s", k, val)
 	}
 	return nil
 }
