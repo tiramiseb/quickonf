3f01811780970873cf3afe968ff9f9c35bb21d33
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-10-28 17:51:18 +0200

    add systemd user services

--- docs/_modules/systemd.md
+++ docs/_modules/systemd.md
@@ -4,10 +4,12 @@ title: Systemd
 
 All instructions in this module need the sudo password to be set. See the [sudo]({% link _modules/sudo.md %}) module.
 
-| Instruction       | Action                   | Arguments                   | Dry run   |
-| ----------------- | ------------------------ | --------------------------- | --------- |
-| `systemd-enable`  | Enable systemd services  | List of services to enable  | No change |
-| `systemd-disable` | Disable systemd services | List of services to disable | No change |
+| Instruction            | Action                        | Arguments                   | Dry run   |
+| ---------------------- | ----------------------------- | --------------------------- | --------- |
+| `systemd-enable`       | Enable systemd services       | List of services to enable  | No change |
+| `systemd-disable`      | Disable systemd services      | List of services to disable | No change |
+| `systemd-user-enable`  | Enable systemd user services  | List of services to enable  | No change |
+| `systemd-user-disable` | Disable systemd user services | List of services to disable | No change |
 
 Example:
 
--- internal/modules/systemd.go
+++ internal/modules/systemd.go
@@ -8,6 +8,8 @@ import (
 func init() {
 	Register("systemd-enable", SystemdEnable)
 	Register("systemd-disable", SystemdDisable)
+	Register("systemd-user-enable", SystemdUserEnable)
+	Register("systemd-user-disable", SystemdUserDisable)
 }
 
 // SystemdEnable enables a systemd service
@@ -49,6 +51,52 @@ func SystemdDisable(in interface{}, out output.Output) error {
 			return err
 		}
 		if _, err := helper.ExecSudo(nil, "", "systemctl", "disable", service); err != nil {
+			return err
+		}
+		out.Successf("Disabled %s", service)
+	}
+	return nil
+}
+
+// SystemdUserEnable enables a user systemd service
+func SystemdUserEnable(in interface{}, out output.Output) error {
+	out.InstructionTitle("Enabling user service")
+	data, err := helper.SliceString(in)
+	if err != nil {
+		return err
+	}
+	for _, service := range data {
+		if Dryrun {
+			out.Infof("Would enable and start %s", service)
+			continue
+		}
+		if _, err := helper.Exec(nil, "", "systemctl", "--user", "enable", service); err != nil {
+			return err
+		}
+		if _, err := helper.Exec(nil, "", "systemctl", "--user", "start", service); err != nil {
+			return err
+		}
+		out.Successf("Enabled %s", service)
+	}
+	return nil
+}
+
+// SystemdUserDisable disables a user systemd service
+func SystemdUserDisable(in interface{}, out output.Output) error {
+	out.InstructionTitle("Disabling user service")
+	data, err := helper.SliceString(in)
+	if err != nil {
+		return err
+	}
+	for _, service := range data {
+		if Dryrun {
+			out.Infof("Would stop and disable %s", service)
+			continue
+		}
+		if _, err := helper.Exec(nil, "", "systemctl", "--user", "stop", service); err != nil {
+			return err
+		}
+		if _, err := helper.Exec(nil, "", "systemctl", "--user", "disable", service); err != nil {
 			return err
 		}
 		out.Successf("Disabled %s", service)
