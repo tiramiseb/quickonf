d089843eb5b1bce8a2d0c4f83cb6a7ac8f1ea1e8
Sébastien Maccagnoni <sebastien.maccagnoni@eclipse-fr.com>
2021-11-01 18:28:19 +0100

    the key may contain store value

--- internal/helper/map_string_interface.go
+++ internal/helper/map_string_interface.go
@@ -8,18 +8,15 @@ import (
 func MapStringInterface(input interface{}) (map[string]interface{}, error) {
 	data, ok := input.(map[string]interface{})
 	if !ok {
-		return nil, errors.New("Module configuration must be a map")
+		return nil, errors.New("module configuration must be a map")
 	}
-	return data, nil
-	/*
-		result := make(map[string]interface{}, len(data))
-		for k, v := range data {
-			key, err := String(k)
-			if err != nil {
-				return nil, err
-			}
-			result[key] = v
+	result := make(map[string]interface{}, len(data))
+	for k, v := range data {
+		key, err := String(k)
+		if err != nil {
+			return nil, err
 		}
-		return result, nil
-	*/
+		result[key] = v
+	}
+	return result, nil
 }
