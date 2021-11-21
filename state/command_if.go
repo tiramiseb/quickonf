1641817de5268ffacbc47c39ae1a38b4946fb818
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-11-22 00:12:37 +0100

    🩹 remove sleep used during dev

--- state/command_if.go
+++ state/command_if.go
@@ -1,8 +1,6 @@
 package state
 
 import (
-	"time"
-
 	"github.com/tiramiseb/quickonf/internal/output"
 )
 
@@ -16,7 +14,6 @@ func (i *If) Run(groupOut *output.Group, vars variables) bool {
 	if !success {
 		out := groupOut.NewInstruction("=")
 		out.Infof(`"%s" is false, not running commands...`, i.Operation.String())
-		time.Sleep(2 * time.Second)
 		return true
 	}
 	for _, cmd := range i.Commands {
