869969b05c81232774d990ef5b70db4669d03caa
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-12-24 21:28:56 +0100

    file.absent

---
+++ internal/commands/file.absent.go
@@ -0,0 +1,51 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func init() {
+	register(fileAbsent)
+}
+
+var fileAbsent = Command{
+	"file.absent",
+	"Make sure a file is absent",
+	[]string{
+		"Absolute path of the file to remove",
+	},
+	nil,
+	`Make sure there is no "Photos" directory for jack\n  file.absent /home/jack/Photos`,
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		path := args[0]
+		if !filepath.IsAbs(path) {
+			return nil, fmt.Sprintf("%s is not an absolute path", path), nil, StatusError
+		}
+		_, err := os.Lstat(path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return nil, fmt.Sprintf("%s does not exist", path), nil, StatusSuccess
+			}
+			return nil, err.Error(), nil, StatusError
+		}
+
+		apply = &Apply{
+			"file.absent",
+			fmt.Sprintf("Will remove %s", path),
+			func(out Output) bool {
+				if err := os.Remove(path); err != nil {
+					out.Errorf("could not remove %s: %s", path, err)
+					return false
+				}
+				out.Successf("Removed %s", path)
+				return true
+			},
+		}
+
+		return nil, fmt.Sprintf("need to remove %s", path), apply, StatusInfo
+
+	},
+	nil,
+}
