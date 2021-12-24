5ae5c788e0f87983df0d3ecbcc9f3077fecf1b4c
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-12-24 23:49:13 +0100

    file

---
+++ internal/commands/file.go
@@ -0,0 +1,66 @@
+package commands
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func init() {
+	register(file)
+}
+
+var file = Command{
+	"file",
+	"Create a file owned by root",
+	[]string{
+		"Absolute path of the file",
+		"Content of the file",
+	},
+	nil,
+	"Say hello\n  file /home/hello.txt \"Hello World!\"",
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		path := args[0]
+		content := args[1]
+		if !filepath.IsAbs(path) {
+			return nil, fmt.Sprintf("%s is not an absolute path", path), nil, StatusError
+		}
+		finfo, err := os.Lstat(path)
+		var existingContent string
+		if err != nil {
+			if !errors.Is(err, fs.ErrNotExist) {
+				return nil, err.Error(), nil, StatusError
+			}
+		} else {
+			if finfo.IsDir() {
+				return nil, fmt.Sprintf("%s is a directory", path), nil, StatusError
+			}
+			bcontent, err := os.ReadFile(path)
+			if err != nil {
+				return nil, err.Error(), nil, StatusError
+			}
+			existingContent = string(bcontent)
+		}
+		if content == existingContent {
+			return nil, fmt.Sprintf("%s already has the requested content", path), nil, StatusSuccess
+		}
+
+		apply = &Apply{
+			"file",
+			fmt.Sprintf("Will write requested content to %s", path),
+			func(out Output) bool {
+				if err := os.WriteFile(path, []byte(content), 0644); err != nil {
+					out.Errorf("Could not write requested content to %s: %s", path, err)
+					return false
+				}
+				out.Successf("Content written to %s", path)
+				return true
+			},
+		}
+
+		return nil, fmt.Sprintf("need to write requested content to %s", path), apply, StatusInfo
+	},
+	nil,
+}
