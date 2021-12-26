5c5d91a2cd48b752b7516e60475d29e36a70612a
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-12-26 22:18:35 +0100

    apt.upgrade

---
+++ internal/commands/apt.upgrade.go
@@ -0,0 +1,47 @@
+package commands
+
+import "github.com/tiramiseb/quickonf/internal/commands/helper"
+
+func init() {
+	register(aptUpgrade)
+}
+
+var aptUpgrade = Command{
+	"apt.upgrade",
+	"Upgrade packages from apt repositories",
+	nil,
+	nil,
+	"Upgrade packages\n  apt.upgrade",
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		apply = &Apply{
+			"apt.upgrade",
+			"Will upgrade apt packages",
+			func(out Output) bool {
+				wait, err := helper.Exec([]string{"DEBIAN_FRONTEND=noninteractive"}, nil, "apt-get", "--yes", "update")
+				if err != nil {
+					out.Errorf("Could not update packages list: %s", err)
+					return false
+				}
+				out.Infof("Updating packages list")
+				if err := wait(); err != nil {
+					out.Errorf("Could not update packages list: %s", err)
+					return false
+				}
+				wait, err = helper.Exec([]string{"DEBIAN_FRONTEND=noninteractive"}, nil, "apt-get", "--yes", "upgrade")
+				if err != nil {
+					out.Errorf("Could not upgrade packages: %s", err)
+					return false
+				}
+				out.Infof("Upgrading packages")
+				if err := wait(); err != nil {
+					out.Errorf("Could not upgrade packages: %s", err)
+					return false
+				}
+				out.Successf("Upgraded all packages")
+				return true
+			},
+		}
+		return nil, "Need to upgrade APT packages", apply, StatusInfo
+	},
+	nil,
+}
