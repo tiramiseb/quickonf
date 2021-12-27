7a6070cabf33d1c94e609caef726e6ce1e8bdf66
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-12-27 09:27:41 +0100

    have the priority suffix for check headers

--- internal/program/specific/check/shorter.go
+++ internal/program/specific/check/shorter.go
@@ -1,6 +1,9 @@
 package check
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/tiramiseb/quickonf/internal/instructions"
 	"github.com/tiramiseb/quickonf/internal/program/common/style"
 )
@@ -8,9 +11,20 @@ import (
 func (m *model) updateGroupname() {
 	if len(m.group.Name) > m.width-3 {
 		m.groupName = m.group.Name[:m.width-3]
-	} else {
-		m.groupName = m.group.Name
+		return
 	}
+	if m.group.Priority != 0 {
+		priorityTag := fmt.Sprintf(" [!%d] ", m.group.Priority)
+		nameAndTag := len(m.group.Name) + len(priorityTag)
+		if nameAndTag > m.width-3 {
+			m.groupName = m.group.Name
+			return
+		}
+		spaces := m.width - 3 - nameAndTag
+		m.groupName = m.group.Name + strings.Repeat(" ", spaces) + priorityTag
+		return
+	}
+	m.groupName = m.group.Name
 }
 
 func (m *model) instructionLine(report instructions.CheckReport) string {
