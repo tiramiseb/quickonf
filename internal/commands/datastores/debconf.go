be06b76e217b717bef18c57fb85d1cd29aa8ebcb
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-12-29 00:04:26 +0100

    debconf.set

---
+++ internal/commands/datastores/debconf.go
@@ -0,0 +1,82 @@
+package datastores
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	"sync"
+)
+
+const debconfDB = "/var/cache/debconf/config.dat"
+
+var (
+	Debconf = debconf{}
+)
+
+type DebconfParameter struct {
+	Name   string
+	Value  string
+	Owners []string
+}
+
+type debconf struct {
+	initOnce   sync.Once
+	parameters []DebconfParameter
+}
+
+func (d *debconf) Get(pkg, name string) (param DebconfParameter, exists bool, err error) {
+	d.initOnce.Do(func() { err = d.init() })
+paramsLoop:
+	for _, p := range d.parameters {
+		if p.Name == name {
+			for _, owner := range p.Owners {
+				if owner == pkg {
+					param = p
+					exists = true
+					break paramsLoop
+				}
+			}
+		}
+	}
+	return
+}
+
+func (d *debconf) Reset() {
+	DpkgMutex.Lock()
+	defer DpkgMutex.Unlock()
+	d.parameters = nil
+	d.initOnce = sync.Once{}
+}
+
+func (d *debconf) init() error {
+	DpkgMutex.Lock()
+	defer DpkgMutex.Unlock()
+	f, err := os.Open(debconfDB)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scanner := bufio.NewScanner(f)
+	param := DebconfParameter{}
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			d.parameters = append(d.parameters, param)
+			param = DebconfParameter{}
+		}
+		info := strings.SplitN(line, ": ", 2)
+		if len(info) != 2 {
+			continue
+		}
+		switch info[0] {
+		case "Name":
+			param.Name = info[1]
+		case "Value":
+			param.Value = info[1]
+		case "Owners":
+			param.Owners = strings.Split(info[1], ", ")
+		}
+	}
+	d.parameters = append(d.parameters, param)
+	return scanner.Err()
+}
---
+++ internal/commands/debconf.set.go
@@ -0,0 +1,79 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/tiramiseb/quickonf/internal/commands/datastores"
+	"github.com/tiramiseb/quickonf/internal/commands/helper"
+)
+
+func init() {
+	register(debconfSet)
+}
+
+var debconfSet = Command{
+	"debconf.set",
+	"Set a debconf parameter",
+	[]string{
+		"Package where the parameter belong",
+		"Name of the parameter",
+		"Value to apply to the parameter",
+	},
+	nil,
+	"Install MS fonts\n  debconf.set ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula true\n  apt.install ttf-mscorefonts-installer",
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		pkg := args[0]
+		name := args[1]
+		value := args[2]
+		param, ok, err := datastores.Debconf.Get(pkg, name)
+		if err != nil {
+			return nil, err.Error(), nil, StatusError
+		}
+		var verb string
+		if ok {
+			if param.Value == value {
+				return nil, fmt.Sprintf("%s already has value %s", name, value), nil, StatusSuccess
+			}
+			verb = "change"
+		} else {
+			verb = "set"
+		}
+		apply = &Apply{
+			"debconf.set",
+			fmt.Sprintf("Will %s %s to %s", verb, name, value),
+			func(out Output) bool {
+				out.Infof("Setting %s to %s", name, value)
+				tmpfile, err := os.CreateTemp("", "quickonf-debconf")
+				if err != nil {
+					out.Errorf("Could not create temporary file: %s", err)
+					return false
+				}
+				defer os.Remove(tmpfile.Name())
+				if _, err := tmpfile.WriteString(fmt.Sprintf("%s %s select %s", pkg, name, value)); err != nil {
+					tmpfile.Close()
+					out.Errorf("Could not write to temporary file: %s", err)
+					return false
+				}
+				if err := tmpfile.Close(); err != nil {
+					out.Errorf("Could not close temporary file: %s", err)
+					return false
+				}
+				wait, err := helper.Exec(nil, nil, "debconf-set-selections", tmpfile.Name())
+				if err != nil {
+					out.Errorf("Could not execute debconf-set-selections: %s", err)
+					return false
+				}
+				if err := wait(); err != nil {
+					out.Errorf("Could not execute debconf-set-selections: %s", err)
+					return false
+				}
+				out.Successf("%s set to %s", name, value)
+				return true
+
+			},
+		}
+		return nil, fmt.Sprintf("Need to %s %s to %s", verb, name, value), apply, StatusInfo
+	},
+	datastores.Debconf.Reset,
+}
