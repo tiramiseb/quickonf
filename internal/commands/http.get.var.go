7cb93113e074f6c4b61471f7d2b402799589ceed
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2021-12-29 20:51:17 +0100

    http.get.var

---
+++ internal/commands/http.get.var.go
@@ -0,0 +1,44 @@
+package commands
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func init() {
+	register(httpGetVar)
+}
+
+var httpGetVar = Command{
+	"http.get.var",
+	"Download content with a HTTP GET request (URI is downloaded when checking)",
+	[]string{
+		"URI of the content to download",
+	},
+	[]string{
+		"Downloaded content",
+	},
+	"Download example\n  foobar = http.get.var http://www.example.com",
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		uri := args[0]
+		resp, err := http.Get(uri)
+		if err != nil {
+			return []string{""}, fmt.Sprintf("Could not download %s: %s", uri, err), nil, StatusError
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == http.StatusNotFound {
+			return []string{""}, fmt.Sprintf("%s not found", uri), nil, StatusError
+		}
+		var buf strings.Builder
+		if _, err := io.Copy(&buf, resp.Body); err != nil {
+			return []string{""}, fmt.Sprintf("Could not read data: %s", err), nil, StatusError
+		}
+		result = []string{
+			buf.String(),
+		}
+		return result, fmt.Sprintf("Downloaded %s", uri), nil, StatusSuccess
+	},
+	nil,
+}
