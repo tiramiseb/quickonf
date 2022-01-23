09b6abec5c08566239eca737c5fce99c2e529a5e
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2022-01-23 19:18:17 +0100

    user.group

---
+++ internal/commands/user.group.go
@@ -0,0 +1,78 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/tiramiseb/quickonf/internal/commands/datastores"
+	"github.com/tiramiseb/quickonf/internal/commands/helper"
+)
+
+func init() {
+	register(userGroup)
+}
+
+var userGroup = Command{
+	"user.group",
+	"Add the user to the group (if the group does not exist, it is created)",
+	[]string{
+		"Username",
+		"Groupname",
+	},
+	nil,
+	"Allow alice to dial with a modem\n  user.group alice dialout",
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		username := args[0]
+		groupname := args[1]
+		user, err := datastores.Users.Get(username)
+		if err != nil {
+			return nil, err.Error(), nil, StatusError
+		}
+		if user.Group.Name == groupname {
+			return nil, fmt.Sprintf("%s is already a member of %s", username, groupname), nil, StatusSuccess
+		}
+		for _, g := range user.Groups {
+			if g.Name == groupname {
+				return nil, fmt.Sprintf("%s is already a member of %s", username, groupname), nil, StatusSuccess
+			}
+		}
+
+		_, exists, err := datastores.Users.GetGroup(groupname)
+		if err != nil {
+			return nil, err.Error(), nil, StatusError
+		}
+
+		var willMessage string
+		var needMessage string
+		if exists {
+			willMessage = fmt.Sprintf("Will create group %s and make %s a member of it", groupname, username)
+			needMessage = fmt.Sprintf("Need to create group %s and make %s a member of it", groupname, username)
+		} else {
+			willMessage = fmt.Sprintf("Will make %s a member of group %s", username, groupname)
+			needMessage = fmt.Sprintf("Need to make %s a member of group %s", username, groupname)
+		}
+
+		apply = &Apply{
+			"user.group",
+			willMessage,
+			func(out Output) bool {
+				if !exists {
+					out.Infof("Creating group %s", groupname)
+					if err := helper.Exec(nil, nil, "groupadd", groupname); err != nil {
+						out.Errorf("Could not create group %s: %s", groupname, err)
+						return false
+					}
+					out.Infof("Created group %s", groupname)
+				}
+				out.Infof("Adding %s to group %s", username, groupname)
+				if err := helper.Exec(nil, nil, "usermod", "--append", "--groups", groupname); err != nil {
+					out.Errorf("Could not add %s to group %s: %s", username, groupname, err)
+					return false
+				}
+				out.Successf("User %s added to group %s", username, groupname)
+				return true
+			},
+		}
+		return nil, needMessage, apply, StatusInfo
+	},
+	datastores.Users.Reset,
+}
