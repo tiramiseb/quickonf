730612920f77b1d61a6c95ee36218f28ec98bd05
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2022-02-03 22:01:53 +0100

    json.get

---
+++ internal/commands/json.get.go
@@ -0,0 +1,35 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/tidwall/gjson"
+)
+
+func init() {
+	register(jsonGet)
+}
+
+var jsonGet = Command{
+	"json.get",
+	"Get a JSON value from a variable",
+	[]string{
+		"Variable containing the JSON object",
+		"Path of the value to get",
+	},
+	[]string{
+		"Value",
+	},
+	"Get a value\n  val = json.get \"{\\\"download\\\":{\\\"url\\\":\\\"http://www.example.com/\\\"}}\" download.url",
+	func(args []string) (result []string, msg string, apply *Apply, status Status) {
+		data := args[0]
+		path := args[1]
+		if ok := gjson.Valid(data); !ok {
+			return []string{""}, "First argument is not a JSON object", nil, StatusError
+		}
+		value := gjson.Get(data, path)
+		str := value.String()
+		return []string{str}, fmt.Sprintf("Read value from JSON: %s", str), nil, StatusInfo
+	},
+	nil,
+}
