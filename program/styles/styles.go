4080fbf9301d008387ee2e1e66787b86c7757b15
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2022-06-26 21:07:46 +0200

    improve error status color (red)

--- program/styles/styles.go
+++ program/styles/styles.go
@@ -13,14 +13,14 @@ var (
 		commands.StatusInfo:    lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#000099")),
 		commands.StatusRunning: lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#555500")),
 		commands.StatusSuccess: lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#005500")),
-		commands.StatusError:   lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#ff0000")),
+		commands.StatusError:   lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#990000")),
 	}
 	SelectedStyles = map[commands.Status]lipgloss.Style{
 		commands.StatusNotRun:  lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#777777")),
 		commands.StatusInfo:    lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#3333bb")),
 		commands.StatusRunning: lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#777722")),
 		commands.StatusSuccess: lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#227722")),
-		commands.StatusError:   lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#ff2222")),
+		commands.StatusError:   lipgloss.NewStyle().Foreground(lipgloss.Color("#ffffff")).Background(lipgloss.Color("#bb3333")),
 	}
 )
 
