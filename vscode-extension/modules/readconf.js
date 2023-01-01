455d266e2c0fb3f51cb2b6bc341d84b1db457ca8
Sébastien Maccagnoni <sebastien@maccagnoni.eu>
2023-01-01 20:06:30 +0100

    slight upgrade to readconf.js

--- vscode-extension/modules/readconf.js
+++ vscode-extension/modules/readconf.js
@@ -1,34 +1,35 @@
 const { spawnSync } = require('child_process')
 const vscode = require('vscode')
 
-const docVersion = {}
-const docsParsers = {}
+const parsedDocs = {}
 
 function callQuickonf(doc) {
 	// Do not process the same doc version twice
-	if (doc.version !== docVersion[doc.uri] || docsParsers[doc.uri]) {
-		docsParsers[doc.uri] = new Promise((resolve) => {
-			docVersion[doc.uri] = doc.version
-			output = spawnSync(
-				vscode.workspace.getConfiguration("quickonf").get("executablePath"),
-				["--check-stdin"],
-				{ input: doc.getText() }
-			)
-			if (output.error) {
-				let message = output.error.message
-				if (output.error.code === "ENOENT") {
-					message = "Error: wrong path for quickonf executable"
-				}
-				vscode.window.showErrorMessage(message, "Open settings").then((opt) => {
-					if (opt === "Open settings") {
-						vscode.commands.executeCommand("workbench.action.openSettings", "quickonf.executablePath")
+	if (!(parsedDocs[doc.uri]) || doc.version !== parsedDocs[doc.uri].version) {
+		parsedDocs[doc.uri] = {
+			version: doc.version,
+			data: new Promise((resolve) => {
+				output = spawnSync(
+					vscode.workspace.getConfiguration("quickonf").get("executablePath"),
+					["--check-stdin"],
+					{ input: doc.getText() }
+				)
+				if (output.error) {
+					let message = output.error.message
+					if (output.error.code === "ENOENT") {
+						message = "Error: wrong path for quickonf executable"
 					}
-				})
-			}
-			resolve(JSON.parse(output.stdout))
-		})
+					vscode.window.showErrorMessage(message, "Open settings").then((opt) => {
+						if (opt === "Open settings") {
+							vscode.commands.executeCommand("workbench.action.openSettings", "quickonf.executablePath")
+						}
+					})
+				}
+				resolve(JSON.parse(output.stdout))
+			}),
+		}
 	}
-	return docsParsers[doc.uri]
+	return parsedDocs[doc.uri].data
 }
 
 module.exports = callQuickonf
